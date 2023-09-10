199ade05755ffc9bf9505121f36edc0cc7fc0eb4
Septian Maulana <septianm028@gmail.com>
2023-09-10 16:17:49 +0700

    feat: define collections for hospital, doctor, specialization and sub-specialization

---
+++ src/api/sub-specialization/controllers/sub-specialization.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * sub-specialization controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::sub-specialization.sub-specialization');
---
+++ src/api/sub-specialization/services/sub-specialization.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * sub-specialization service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::sub-specialization.sub-specialization');
